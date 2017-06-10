f2bd23e87cad69d3953d6db8837bcc787cd312c7
saranthn <saranthn@gmail.com>
2017-06-10 09:00:23 +0530

    added bow and arrow

--- Dtask1.js
+++ Dtask1.js
@@ -6,7 +6,7 @@ var bow;
 
 function startGame() {
   board = new drawComponent(70);
-  bow = new drawBow();
+  bow = new drawBow(250);
   myGameArea.start();
 
 }
@@ -20,10 +20,38 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+          myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+}
+
+function drawBow(lineStart) {
+  this.lineStart = lineStart;
+  this.speedY = 0;
+  this.update = function () {
+    var ctx = myGameArea.context;
+    ctx.moveTo(70,this.lineStart);
+    ctx.lineTo(70,this.lineStart+100);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(20,this.lineStart+50,70,7*Math.PI/4,Math.PI/4);
+    ctx.stroke();
+    ctx.moveTo(60,this.lineStart+50);
+    ctx.lineTo(110,this.lineStart+50);
+    ctx.stroke();
+
+  }
+  this.newPos = function() {
+      this.lineStart += this.speedY;
+  }
+
 }
 
 function drawComponent(y) {
@@ -62,5 +90,11 @@ function updateGameArea() {
     board.cenheight+=2;
     if(board.cenheight==600)
     board.cenheight = 0;
+
+    bow.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 38) {bow.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {bow.speedY = 1; }
     board.update();
+    bow.newPos();
+    bow.update();
 }
