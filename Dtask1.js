443787e9d660866b5d2aa71cb1737f5e6b7d7b49
saranthn <saranthn@gmail.com>
2017-06-12 12:27:58 +0530

    added points and pause button

--- Dtask1.js
+++ Dtask1.js
@@ -5,9 +5,12 @@ var bow;
 var arrow;
 var noarrows = 0;
 var prevnoarrows = 0;
+var points = 0;
+var pausedGame = 0;
 
 
 function startGame() {
+
   board = new drawComponent(70);
   bow = new drawBow(250);
   arrow = new drawArrow(60,250);
@@ -20,7 +23,7 @@ var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = 1350;
-        this.canvas.height = 600;
+        this.canvas.height = 540;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -36,6 +39,16 @@ var myGameArea = {
     },
     stop : function() {
         clearInterval(this.interval);
+    },
+    pause : function () {
+            if (!pausedGame) {
+            clearInterval(this.interval);
+            pausedGame= 1;
+            }
+            else if (pausedGame) {
+              this.interval = setInterval(updateGameArea, 20);
+            pausedGame = 0;
+            }
     }
 }
 
@@ -52,21 +65,50 @@ function drawArrow(x,y) {
   }
   this.newPos = function (){
       this.x += this.speedX;
-      this.y += this.speedY;
+     this.y += this.speedY;
+
+
       if(this.x == 1350){
-        this.x = 70;
+        this.x = 60;
         noarrows++;
       }
 
   }
-  this.crashWith = function(otherobj) {
+
+
+  this.crashWith = function(board) {
         var arrowRight = this.x + 50;
         var arrowHeight = this.y + 50;
-        var boardCenter = otherobj.cenheight;
+        var boardCenter = board.cenheight;
         var crash = false;
-        if ((arrowRight<1150)&&(arrowRight>1050)&&(arrowHeight<boardCenter+50)&&(arrowHeight>boardCenter-50)) {
-           crash = true;
+        if(arrowRight == 1100){
+          if (arrowHeight<board.cenheight+10&&arrowHeight>board.cenheight-10) {
+             crash = true;
+             points +=100;
+          }
+          else if (arrowHeight<board.cenheight+20&&arrowHeight>board.cenheight-20) {
+             crash = true;
+             points +=50;
+          }
+          else if (arrowHeight<board.cenheight+30&&arrowHeight>board.cenheight-30) {
+             crash = true;
+             points+=40;
+          }
+          else if (arrowHeight<board.cenheight+40&&arrowHeight>board.cenheight-40) {
+             crash = true;
+             points +=30;
+          }
+          else if (arrowHeight<board.cenheight+50&&arrowHeight>board.cenheight-50) {
+             crash = true;
+             points +=20;
+          }
+          else if (arrowHeight<board.cenheight+60&&arrowHeight>board.cenheight-60) {
+             crash = true;
+             points +=10;
+          }
         }
+
+
         return crash;
     }
 
@@ -96,52 +138,51 @@ function drawBow(lineStart) {
 
 function drawComponent(y) {
   this.cenheight = y;
+  this.speedY = 5;
   this.update = function () {
-    var ctx = myGameArea.context;
-    ctx.beginPath();
-    ctx.arc(1100,this.cenheight,60,0,2*Math.PI);
-    ctx.fillStyle = "#00A2FF";
-    ctx.fill();
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(1100,this.cenheight,50,0,2*Math.PI);
-    ctx.fillStyle = "#E5C900";
-    ctx.fill();
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(1100,this.cenheight,40,0,2*Math.PI);
-    ctx.fillStyle = "#FF6800";
-    ctx.fill();
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(1100,this.cenheight,30,0,2*Math.PI);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill();
-    ctx.stroke();
+    this.cenheight += this.speedY;
+    if((this.cenheight+60) == myGameArea.canvas.height ||(this.cenheight-50)==0){
+      this.speedY = -this.speedY;
+    }
+    drawCircle(this.cenheight,60,"#000000");
+    drawCircle(this.cenheight,50,"#00A2FF");
+    drawCircle(this.cenheight,40,"#E5C900");
+    drawCircle(this.cenheight,30,"#FF6800");
+    drawCircle(this.cenheight,20,"#FF0000");
+    drawCircle(this.cenheight,10,"#B41010");
 
   }
+}
 
-
+function drawCircle(c,r,color) {
+  var ctx = myGameArea.context;
+  ctx.beginPath();
+  ctx.arc(1100,c,r,0,2*Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.stroke();
 }
 
 
 function updateGameArea() {
-
-    board.cenheight+=2;
-    if(board.cenheight==600)
-    board.cenheight = 0;
+  myGameArea.clear();
+document.getElementById("score").innerText ="POINTS : "+points;
     arrow.speedY = 0;
     bow.speedY = 0;
-    if (myGameArea.key && myGameArea.key == 38) {
-      bow.speedY = -1;
-      arrow.speedY = -1;
+    if (myGameArea.key && myGameArea.key == 38 ) {
+      if(arrow.x == 60){
+        bow.speedY = -7;
+        arrow.speedY = -7;
+      }
    }
     if (myGameArea.key && myGameArea.key == 40) {
-      bow.speedY = 1;
-      arrow.speedY = 1;
+      if(arrow.x == 60){
+        bow.speedY = 7;
+        arrow.speedY = 7;
+      }
    }
     if (myGameArea.key && myGameArea.key == 32) {
-      arrow.speedX = 10;
+      arrow.speedX = 15;
    }
    if(noarrows!=prevnoarrows){
      arrow.speedX = 0;
@@ -149,15 +190,15 @@ function updateGameArea() {
    }
 
    if(arrow.crashWith(board)){
-     myGameArea.stop();
+     arrow.x = 60;
    }
    else{
-          myGameArea.clear();
+
           board.update();
           arrow.newPos();
           bow.newPos();
           bow.update();
           arrow.update();
-   }
+  }
 
 }
